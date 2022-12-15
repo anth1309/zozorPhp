b772352b94a827781b1a1e7e0ed2cfbe50a5cf6c
Anthony CHAMBON <acdf-ac.cdf@hotmail.fr>
2022-12-15 11:10:59 +0100

    possibilite de creer un nouvel user

--- zozor-acceuil.php
+++ zozor-acceuil.php
@@ -1,18 +1,28 @@
+<?php
+
+if (!isset($_SESSION['loguser'])) {
+  header('Location: zozor.php');
+}
+
+
+?>
+
 <head>
   <meta charset="utf-8">
   <title>zozor acceuil</title>
-
   <link rel="stylesheet" href="style-zozor.css">
 </head>
 
 
 <header>
-
   <?php include('header.php'); ?>
 </header>
-<h1>Bienvenu a l'acceuil</h1>
+
 <a id="retourzozor" href="zozor.php">retour a zozor</a>
+<h1>Bienvenu a l'acceuil</h1>
+
 <img src="https://picsum.photos/1500/2000?random=1" />
+
 <footer>
   <?php include('./footer.php'); ?>
 </footer>
